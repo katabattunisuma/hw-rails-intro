c39e4d9a385c09dd3c12e349f9919e0bfabd95f5
Ubuntu <ubuntu@ip-172-31-41-146.us-east-2.compute.internal>
2021-10-01 00:35:55 +0000

    testing step 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,56 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      ratings = params[:ratings]
+   @all_ratings = Movie.all_ratings
+
+    if (request.referrer).nil?
+      session.clear
+    end
+
+    #Store current rating and sort parameters in session to be remembered
+    if !params[:ratings].nil?
+      session[:ratings] = ratings 
+    end
+
+    if !params[:sort].nil?
+      session[:sort] = params[:sort]
+    end
+
+    sort_by = params[:sort]
+    #When all boxes are unchecked we want to display as all ratings are checked
+    if (params[:ratings].nil? and params[:commit]=="Refresh")
+      @ratings_to_show = Movie.all_ratings
+      @movies = Movie.used_ratings(@ratings_to_show, session[:sort])
+      session[:ratings] = params[:rating]
+    #When returning from another pager it should remember the ratings/sort 
+    elsif (params[:ratings].nil? && !session[:ratings].nil?) || (params[:sort].nil? && !session[:sort].nil?)
+      redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
+
+    else
+      if !params[:ratings].nil?
+        ratings = params[:ratings].keys
+      else
+        ratings = @all_ratings
+      end
+      if sort_by == 'title'
+        @sort_by = sort_by
+        @selected = 'title'
+      elsif sort_by=='release_date'
+        @sort_by = sort_by
+        @selected = 'release_date'
+      else
+        @sort_by = ""
+        @ratings_to_show = ratings
+        @selected = nil
+      end
+
+      @ratings_to_show = ratings
+      @movies = Movie.used_ratings(@ratings_to_show, @sort_by)
+
+
+    end
+    
     end
   
     def new
@@ -15,7 +64,7 @@ class MoviesController < ApplicationController
     end
   
     def create
-      @movie = Movie.create!(movie_params)
+      @movie = Movie.create!(params1)
       flash[:notice] = "#{@movie.title} was successfully created."
       redirect_to movies_path
     end
@@ -26,7 +75,7 @@ class MoviesController < ApplicationController
   
     def update
       @movie = Movie.find params[:id]
-      @movie.update_attributes!(movie_params)
+      @movie.update_attributes!(params1)
       flash[:notice] = "#{@movie.title} was successfully updated."
       redirect_to movie_path(@movie)
     end
@@ -44,4 +93,4 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
-  end
+end
